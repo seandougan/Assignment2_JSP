6504a558eb2370cc46ab1c60b7d7fa678f3b1f25
seandougan <mediauthority@gmail.com>
2018-01-03 18:20:27 -0500

    Database connection, login.jsp(seans), web.xml welcome file list

---
+++ Assignment2_JSP/WebContent/Login.jsp
@@ -0,0 +1,27 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<!-- ############################################################################################################################### -->
+<head>
+
+<!-- BOOTSTRAP CSS -->
+<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+ rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" 
+ crossorigin="anonymous">
+ 
+ <!--  META -->
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Company Login</title>
+</head>
+<!-- ################################################################################################################################ -->
+<body>
+
+<h1></h1>
+
+
+<!-- BOOTSTRAP JS -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" 
+integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</body>
+</html>
--- Assignment2_JSP/src/utility/Database.java
+++ Assignment2_JSP/src/utility/Database.java
@@ -1,5 +1,49 @@
+/*
+ * Date: January 4th, 2018,
+ * Group Members: Sean Dougan - 101029633, Matthew McCarthy - 101009842, Michael Van Dyke - 101008864
+ * Gregory Uchitel - 100322200, James Massle - 101025072
+ * Class Description: This class is meant to manage a singleton connection to the database, opening and closing
+ * the connection
+ */
+
 package utility;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 public class Database {
 
+	//DATABASE CONNECTION STRINGS
+	private final static String username = "admin";
+	private final static String password = "admin";
+	private final static String database = "comp3095";
+	private static Connection connect;
+	
+	
+	public static Connection startConnection() {
+		
+		try {
+		      // This will load the MySQL driver, each DB has its own driver
+			System.out.println("Initializing MYSql Driver JAR...");
+		      Class.forName("com.mysql.jdbc.Driver");
+		      
+		      // Setup the connection with the DB
+		      System.out.println("Driver found! Establishing Connection ...");
+		      connect = DriverManager
+			          .getConnection("jdbc:mysql://localhost:3306/"+database+"?"
+			              + "user="+username+"&password="+password);
+		      
+		      System.out.println("Connection Established");
+		      return connect;
+		    } 
+		catch (Exception e) {
+		      
+		    	System.out.println("There is a problem connecting to the MYSql database");
+		    	return null;
+		    } 
+	}
+	
+	
+	
+	
 }
