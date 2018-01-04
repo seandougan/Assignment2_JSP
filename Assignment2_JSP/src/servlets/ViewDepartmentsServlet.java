fb18cad9af455750901404298a03993c94eafd6a
mccarm416 <mccarm416@gmail.com>
2018-01-03 19:09:20 -0500

    Added table, home button, and centerd items on ViewDepartments.jsp

--- Assignment2_JSP/src/servlets/ViewDepartmentsServlet.java
+++ Assignment2_JSP/src/servlets/ViewDepartmentsServlet.java
@@ -20,6 +20,7 @@ public class ViewDepartmentsServlet extends HttpServlet {
     }
     
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("/WEB-INF/Homepage.jsp").forward(request, response);
 	}
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
